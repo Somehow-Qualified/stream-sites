6ca594d8df383df3ac616941aaa90ecd4b0732f5
Melanie E Magdalena <hello@melaniemagdalena.com>
2019-04-07 23:06:53 -0500

    [CMS] delete the nulls, see if this fixes the img error

--- site/admin/preview-templates/blog.js
+++ site/admin/preview-templates/blog.js
@@ -10,20 +10,14 @@ const Blog = createClass({
 
     return html`
       <article>
-        <h1>${entry.getIn(["data", "title"], null)}</h1>
-        <p><time
-              >${
-                format(
-                  entry.getIn(["data", "date"], new Date()),
-                  "DD MMM YYYY"
-                )
-              }</time
-            >
-            — Posted in ${entry.getIn(["data", "tags"], "")}
+        <h1>${entry.getIn(["data", "title"], "")}</h1>
+        <p>
+          <time>${format(entry.getIn(["data", "date"], new Date()),"DD MMM YYYY")}</time>
+          — Posted in ${entry.getIn(["data", "tags"], "")}
         </p>
         <figure>
-          <img src="${entry.getIn(["data", "featured_image"], null)}" alt="${entry.getIn(["data", "image_caption"], null)}">
-          <figcaption>${entry.getIn(["data", "image_caption"], null)}</figcaption>
+          <img src="${entry.getIn(["data", "featured_image"], "")}" alt="${entry.getIn(["data", "image_caption"], "")}">
+          <figcaption>${entry.getIn(["data", "image_caption"], "")}</figcaption>
         </figure>
         <p>TL;DR - ${entry.getIn(["data", "tldr"], "")}</p>
         ${this.props.widgetFor("body")}
