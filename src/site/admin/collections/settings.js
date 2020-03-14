f9c3e8ff123677f05f482eac3755b52d0005738a
Melanie E Magdalena <hello@melaniemagdalena.com>
2020-03-14 01:21:17 -0500

    Tidy up fields

--- src/site/admin/collections/settings.js
+++ src/site/admin/collections/settings.js
@@ -10,16 +10,16 @@ export default {
   },
   files: [
     {
-      label: 'Branding',
+      label: 'Site & Branding',
       name: 'site',
       file: 'src/site/_globals/site.json',
       preview: false,
       fields: [
         {
-          label: 'Brand Name',
+          label: 'Site Name',
           name: 'name',
           widget: 'string',
-          hint: 'The name of your brand, community, or just your Twitch name.'
+          hint: 'The name of your brand, community, or just your Twitch name. Used in your Main Menu and for Search Engines.'
         },
         {
           label: 'Site Description',
@@ -36,13 +36,45 @@ export default {
         },
         {
           label: 'Email address',
-          name: 'email',
+          name: 'author_email',
           widget: 'string',
-          hint: 'This is required for your privacy policy. Where should people reach you? Don\'t have an email - Sidemail is great!'
+          hint: 'Where should people reach you? Don\'t have an email - Sidemail is great!'
+        },
+        {
+          label: 'Email address for privacy policy',
+          name: 'privacy_email',
+          widget: 'string',
+          hint: 'This is required for your privacy policy. Where should people reach you with concerns?'
+        },
+        {
+          label: 'Profile Name',
+          name: 'author_name',
+          widget: 'string',
+          required: false,
+          hint: 'Can be used on posts microformats. What should people call you?'
+        },
+        {
+          label: 'Profile Image',
+          name: 'author_avatar',
+          widget: 'image',
+          media_library: {
+            config: {
+              multiple: false
+            }
+          },
+          required: false,
+          hint: 'Can be used on blog posts and in for microformats.'
+        },
+        {
+          label: 'Mini Bio',
+          name: 'author_bio',
+          widget: 'string',
+          required: false,
+          hint: 'Can be used on posts microformats. Describe yourself in 280 characters or less (like a Tweet).'
         },
         {
           label: 'Twitter Handle',
-          name: 'twitter_author',
+          name: 'author_twitter',
           widget: 'string',
           required: false,
           hint: 'Example: @m2creates. Include the @! Give yourself credit for your work.'
@@ -55,45 +87,11 @@ export default {
           hint: 'Enter your Twitch handle.'
         },
         {
-          label: 'Theme',
-          name: 'colors',
-          widget: 'object',
-          fields: [
-            {
-              label: 'Main Color',
-              name: 'main',
-              widget: 'select',
-              'default': 'purple',
-              options: [
-                'red',
-                'orange',
-                'yellow',
-                'teal',
-                'blue',
-                'indigo',
-                'purple',
-                'pink'
-              ],
-              hint: 'Pick a main color for your site.'
-            },
-            {
-              label: 'Accent Color',
-              name: 'accent',
-              widget: 'select',
-              'default': 'pink',
-              options: [
-                'red',
-                'orange',
-                'yellow',
-                'teal',
-                'blue',
-                'indigo',
-                'purple',
-                'pink'
-              ],
-              hint: 'Pick a secondary color for your site. Used on buttons and other accents.'
-            }
-          ]
+          label: 'Twitch Client ID',
+          name: 'twitch_client',
+          widget: 'string',
+          required: false,
+          hint: 'Enter your Client ID from Twitch Developers to enable live alerts.'
         },
         {
           label: 'Logo',
@@ -136,6 +134,124 @@ export default {
           widget: 'string',
           required: false,
           hint: 'Describe the site image for those who can\'t see it.'
+        }
+      ]
+    },
+    {
+      label: 'Theme Settings',
+      name: 'theme',
+      file: 'src/site/_globals/theme.json',
+      preview: false,
+      fields: [
+        {
+          label: 'Theme',
+          name: 'theme',
+          widget: 'string',
+          'default': 'ghostwind',
+          hint: 'Enter the name of your theme. This is case sensitive, all lowercase, with no spaces. For example: ghostwind or skeleventy.'
+        },
+        {
+          label: 'Theme Colors',
+          name: 'colors',
+          widget: 'object',
+          fields: [
+            {
+              label: 'Main Color',
+              name: 'main',
+              widget: 'select',
+              'default': 'purple',
+              options: [
+                'red',
+                'orange',
+                'yellow',
+                'teal',
+                'blue',
+                'indigo',
+                'purple',
+                'pink'
+              ],
+              hint: 'Pick a main color for your site.'
+            },
+            {
+              label: 'Accent Color',
+              name: 'accent',
+              widget: 'select',
+              'default': 'pink',
+              options: [
+                'red',
+                'orange',
+                'yellow',
+                'teal',
+                'blue',
+                'indigo',
+                'purple',
+                'pink'
+              ],
+              hint: 'Pick a secondary color for your site. Used on buttons and other accents.'
+            }
+          ]
+        },
+        {
+          label: 'About Page',
+          name: 'about',
+          widget: 'object',
+          fields: [
+            {
+              label: 'Heading',
+              name: 'heading',
+              widget: 'string',
+              'default': 'About',
+              hint: 'The page title for /about'
+            },
+            {
+              label: 'Sub Heading',
+              name: 'subheading',
+              widget: 'string',
+              'default': 'Nice to meet you',
+              hint: 'A subtitle for your heading used in some themes.'
+            },
+            {
+              label: 'Slug',
+              name: 'slug',
+              widget: 'string',
+              'default': 'about',
+              hint: 'Default: about'
+            },
+            {
+              label: 'Card Image',
+              name: 'image_card',
+              widget: 'image',
+              required: false,
+              media_library: {
+                config: {
+                  multiple: false
+                }
+              },
+              required: false,
+              hint: 'A default image for sharing on Twitter/Facebook.'
+            },
+            {
+              label: 'Card Image Alt Text',
+              name: 'image_card_alt',
+              widget: 'string',
+              required: false,
+              hint: 'Describe the site image for those who can\'t see it.'
+            },
+            {
+              label: 'SEO Title',
+              name: 'seo_title',
+              widget: 'string',
+              required: false,
+              hint: 'A page title for search engines.'
+            },
+            {
+              label: 'SEO Description',
+              name: 'seo_desc',
+              widget: 'text',
+              required: false,
+              hint: 'Write a description to appear on via search results and the social media card preview.'
+            }
+          ]
         }
       ]
     },
