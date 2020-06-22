6d86f08b5f6e861d567cd913458985900545d310
Melanie E Magdalena <hello@melaniemagdalena.com>
2020-06-21 19:15:45 -0500

    wip: Webmentions

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -12,6 +12,9 @@ TODO: Create a filter for popular tags, and use `limit` to choose how many; prob
 */
 
 const { DateTime } = require('luxon');
+const siteData = require('../site/_data/site.json');
+const rootUrl = siteData.url
+const twitter = siteData.author_twitter
 
 module.exports = {
 
@@ -128,5 +131,40 @@ module.exports = {
   cacheBuster: value => {
     let milliseconds = Date.now();
     return value + "?rev=" + milliseconds;
+  },
+
+  /**
+   * Webmentions by sia.codes
+   */
+  getWebmentionsForUrl: (webmentions, url) => {
+    return webmentions.children.filter(entry => entry['wm-target'] === url);
+  },
+  isOwnWebmention: (webmention) => {
+    const urls = [
+     `${rootUrl}`,
+     `${twitter}`
+    ]
+    const authorUrl = webmention.author ? webmention.author.url : false;
+    // check if a given URL is part of this site.
+    return authorUrl && urls.includes(authorUrl);
+  },
+  size: (mentions) => {
+    return !mentions ? 0 : mentions.length;
+  },
+  webmentionsByType: (mentions, mentionType) => {
+    return mentions.filter(entry => !!entry[mentionType]);
+  },
+  readableDateFromISO: (dateStr, formatStr = "dd LLL yyyy 'at' hh:mma") => {
+    return DateTime.fromISO(dateStr).toFormat(formatStr);
+  },
+  generateShareLink: (url, text) => {
+    const shareText = `${text} by ${twitter}`;
+    const shareUrl = `${rootUrl}${url}`;
+    return `https://twitter.com/intent/tweet/?text=${encodeURI(shareText)}&amp;url=${encodeURI(shareUrl)}`;
+  },
+  generateDiscussionLink: (url) => {
+    const postUrl = `${rootUrl}${url}`
+    return `https://twitter.com/search?f=tweets&src=typd&q=${encodeURI(postUrl)}`;
   }
+
 }
