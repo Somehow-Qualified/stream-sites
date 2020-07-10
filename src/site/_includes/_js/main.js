d1d6dc3d54332b85ab74c8e005f3533d8245c669
Melanie E Magdalena <hello@melaniemagdalena.com>
2020-07-10 11:41:31 -0500

    clean up

--- src/site/_includes/_js/main.js
+++ src/site/_includes/_js/main.js
@@ -1,75 +1,63 @@
 // Dark Mode
 
-const SELECTORS = {
-  toggleBtn: '.js-darkmode-toggle'
-}
-const CLASSES = {
-  darkMode: 'mode-dark'
-}
-
-const STORAGE_KEY = 'darkMode'
-const COLOR_SCHEME_KEY = '--color-scheme'
+const SELECTORS = { toggleBtn: '.js-darkmode-toggle' }
+const CLASSES = { darkMode: 'mode-dark' }
+const STORAGE_KEY = 'darkMode';
+const COLOR_SCHEME_KEY = '--color-scheme';
 
 class DarkMode {
   constructor() {
-    this.toggleBtn = document.querySelector(SELECTORS.toggleBtn)
-    this.isActive = false
-
+    this.toggleBtn = document.querySelector(SELECTORS.toggleBtn);
+    this.isActive = false;
     if (this.toggleBtn) {
-      this.toggleBtn.addEventListener('click', () => this.toggle())
+      this.toggleBtn.addEventListener('click', () => this.toggle());
     }
-
-    this.init()
+    this.init();
   }
 
   getSystemPreference() {
     let response = getComputedStyle(
       document.documentElement
-    ).getPropertyValue(COLOR_SCHEME_KEY)
-
+    ).getPropertyValue(COLOR_SCHEME_KEY);
     if (response.length) {
-      return response.replace(/\"/g, '').trim()
+      return response.replace(/\"/g, '').trim();
     }
-
-    return null
+    return null;
   }
 
   init() {
     if (this.hasLocalStorage()) {
-      const systemPreference = this.getSystemPreference()
-      const storedSetting = localStorage.getItem(STORAGE_KEY)
-
-      let preference
+      const systemPreference = this.getSystemPreference();
+      const storedSetting = localStorage.getItem(STORAGE_KEY);
+      let preference;
       if (storedSetting) {
-        preference = storedSetting === 'true'
+        preference = storedSetting === 'true';
       } else if (systemPreference) {
-        preference = systemPreference === 'dark'
+        preference = systemPreference === 'dark';
       }
-
-      this.toggle(preference)
+      this.toggle(preference);
     }
   }
 
   toggle(force) {
-    this.isActive = typeof force === 'boolean' ? force : !this.isActive
+    this.isActive = typeof force === 'boolean' ? force : !this.isActive;
     document.documentElement.classList.toggle(
       CLASSES.darkMode,
       this.isActive
     )
-    this.toggleBtn.setAttribute('aria-checked', String(this.isActive))
-
+    this.toggleBtn.setAttribute('aria-checked', String(this.isActive));
     if (this.hasLocalStorage()) {
-      localStorage.setItem(STORAGE_KEY, this.isActive)
+      localStorage.setItem(STORAGE_KEY, this.isActive);
     }
   }
 
   hasLocalStorage() {
-    return typeof Storage !== 'undefined'
+    return typeof Storage !== 'undefined';
   }
 }
 
 if (window.CSS && CSS.supports('color', 'var(--fake-var)')) {
-  new DarkMode()
+  new DarkMode();
 }
 
 /*
