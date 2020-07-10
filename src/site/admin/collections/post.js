cc40fd6b0487028868a4137fc353161d611b77f3
Melanie E Magdalena <hello@melaniemagdalena.com>
2020-07-10 10:51:06 -0500

    Add hint to category explaining current limitation

--- src/site/admin/collections/post.js
+++ src/site/admin/collections/post.js
@@ -62,13 +62,14 @@ export default {
       name: 'category',
       widget: 'string',
       required: false,
+      hint: 'Apply a category to a post. Supports 1 category at this time.'
     },
     {
       label: 'Tag',
       name: 'tags',
       widget: 'list',
       required: false,
-      hint: 'We recommend what game it\'s of. Apply tags to create archives of posts that are similar and/or related. Separate multiple with a comma: i.e. Tag 1, Tag 2, Tag 3'
+      hint: 'Apply tags to create archives of posts that are similar and/or related. Separate multiple with a comma: i.e. Tag 1, Tag 2, Tag 3.'
     },
     {
       label: 'Header Image or Video Thumbnail',
