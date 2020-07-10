54f4993974dec54d5da8b87f565bfef495be44dc
Melanie E Magdalena <hello@melaniemagdalena.com>
2020-07-10 09:51:19 -0500

    fix: categories with fatsackfails edits

--- src/utils/collections.js
+++ src/utils/collections.js
@@ -63,20 +63,20 @@ module.exports = {
 
   // Return a collection for each Category
   category: collection => {
-    let categories = {}
+    let categories = {};
     collection.getAllSorted().forEach(item => {
-
-      let category = item.data.category;
-
-      if (typeof category !== "string")
-        return
-      if (Array.isArray(categories[category]))
-        categories[category].push(item)
-      else
-        categories[category] = [item]
-    })
-
-    return categories
+      let category = item.data.category
+      if (typeof category !== "string"){
+        return;
+      }
+      if (Array.isArray(categories[category])){
+        categories[category].push(item);
+      }
+      else {
+        categories[category] = [item];
+      }
+    });
+    return categories;
   },
 
   // Posts
