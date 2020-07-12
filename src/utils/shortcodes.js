7278c3e9793b2bee83caad80adf5b1a5648f718b
Melanie E Magdalena <hello@melaniemagdalena.com>
2020-07-12 07:49:07 -0500

    docs(shortcodes): define breakoutImage parameters with example

--- src/utils/shortcodes.js
+++ src/utils/shortcodes.js
@@ -1,8 +1,19 @@
 module.exports = {
 
-  // {% breakoutImage "/images/og.jpg", "An image test" %}
-  breakoutImage: (image, image_alt) => {
-    return `<div class="breakout"><img src="${image}" alt="${image_alt}" class="w-full h-96 object-cover" /></div>`;
+  /*
+   * Breakout Image
+   *
+   * Definitions:
+   * - image: path to file or URL
+   * - image_alt: plain text string
+   * - height: TailwindCSS class, h-{size}
+   *
+   * Usage:
+   * {% breakoutImage "/images/og.jpg", "An image test", "h-96" %}
+   *
+   */
+  breakoutImage: (image, image_alt, height) => {
+    return `<div class="breakout"><img src="${image}" alt="${image_alt}" class="w-full ${height} object-cover" /></div>`;
   }
 
 }
